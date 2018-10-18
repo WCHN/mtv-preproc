3dd798947cf48431dcbf452f21833768e4a29360
Mikael Brudfors <brudfors@gmail.com>
2018-10-18 14:39:27 +0100

    Delete compile_mex.m

--- private/compile_mex.m
+++
@@ -1,4 +0,0 @@
-function compile_mex(fname_mex)
-f = fileparts(which('compile_mex'));
-mex(fullfile(f,fname_mex),'-outdir',f);
-%==========================================================================
