8a4664b7c024a41549f24a002174d32be6d0ef7b
Yaël Balbastre <yael.balbastre@gmail.com>
2018-11-15 20:26:34 +0000

    Feature: Backward-Forward gradients

--- private/imgrad.m
+++ private/imgrad.m
@@ -1,13 +1,61 @@
-function G = imgrad(im,vx) 
-% Compute image gradient
-%__________________________________________________________________________
-% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging   
-
-if nargin < 2, vx = ones([1 3],'like',im); end
-
-Gx = cat(2, diff(im,1,2), zeros(size(im,1),1,size(im,3), 'like', im))./vx(1);
-Gy = cat(1, diff(im,1,1), zeros(1,size(im,2),size(im,3), 'like', im))./vx(2);
-Gz = cat(3, diff(im,1,3), zeros(size(im,1),size(im,2),1, 'like', im))./vx(3);  
-
-G = cat(4,Gx,Gy,Gz);
 %==========================================================================
+function D = imgrad(X,vs,type) 
+% Calculate spatial gradient of an image (with voxel size)
+%
+% FORMAT grad = spm_imbasics('grad',img,(vs),(type))
+% img   - Image (Nx * Nz * Nz * ...)
+% vs    - voxel size [1]
+% type  - Finite difference type ['+']/'-'/'+-'/'-+'
+% grad  - Gradients in all direction (Nx * Nz * Nz * ... * Ndim * Ntype)
+%__________________________________________________________________________
+% Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
+
+% -------------------------------------------------------------------------
+% Default parameters
+if nargin < 2, vs = ones(1,'like',X); end
+if nargin < 3, type = '+-'; end
+
+dim    = size(X);
+dimout = [numel(type) numel(dim) prod(dim)];
+vs     = padarray(vs(:)', [0 max(0,3-numel(dim))], 'replicate', 'post');
+
+% -------------------------------------------------------------------------
+% Compute each derivative that is asked for.
+% The results of diff is of length N-1, so we pad the result with a zero.
+D = zeros(dimout, 'like', X);
+for i=1:numel(dim)
+    for t=1:numel(type)
+        if dim(i)  > 1
+            dimpad       = dim;
+            dimpad(i)    = 1;
+            switch type(t)
+                case '+'
+                    D(t,i,:) = reshape(cat(i, diff(X,1,i), zeros(dimpad, 'like', X)), 1, 1, []);
+                case '-'
+                    D(t,i,:) = reshape(cat(i, zeros(dimpad, 'like', X), diff(X,1,i)), 1, 1, []);
+            end
+            D(t,i,:) = D(t,i,:) ./ vs(i);
+        end
+    end
+end
+
+% -------------------------------------------------------------------------
+% Reorder dimensions > Nx * Nz * Nz * ... * Ndim * Ntype
+D = permute(D, [3 2 1]);
+D = reshape(D, [dim numel(dim) numel(type)]);
+D = D/sqrt(numel(type));
+%==========================================================================
+
+% function G = imgrad(im,vx) 
+% % Compute image gradient
+% %__________________________________________________________________________
+% % Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging   
+% 
+% if nargin < 2, vx = ones([1 3],'like',im); end
+% 
+% Gx = cat(2, diff(im,1,2), zeros(size(im,1),1,size(im,3), 'like', im))./vx(1);
+% Gy = cat(1, diff(im,1,1), zeros(1,size(im,2),size(im,3), 'like', im))./vx(2);
+% Gz = cat(3, diff(im,1,3), zeros(size(im,1),size(im,2),1, 'like', im))./vx(3);  
+% 
+% G = cat(4,Gx,Gy,Gz);
+% %==========================================================================
--- private/laplace_prior.m
+++ private/laplace_prior.m
@@ -3,25 +3,27 @@ function L = laplace_prior(dm,vx)
 % _______________________________________________________________________
 %  Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
 
-L = zeros(dm);
-if dm(1)>=2
-    tmp        = 1/(vx(1)^2);
-    L(  1,1,1) = L(  1,1,1) + tmp*2;
-    L(  2,1,1) = L(  2,1,1) - tmp;
-    L(end,1,1) = L(end,1,1) - tmp;
-end
-if dm(2)>=2
-    tmp        = 1/(vx(2)^2);
-    L(1,  1,1) = L(1,  1,1) + tmp*2;
-    L(1,  2,1) = L(1,  2,1) - tmp;
-    L(1,end,1) = L(1,end,1) - tmp;
-end
-if dm(3)>=2
-    tmp        = 1/(vx(3)^2);
-    L(1,1,  1) = L(1,1,  1) + tmp*2;
-    L(1,1,  2) = L(1,1,  2) - tmp;
-    L(1,1,end) = L(1,1,end) - tmp;
-end
-L = single(fftn(L));
-L = @(Y) real(ifftn(fftn(Y).*L));
+L = @(Y) spm_field('vel2mom', single(Y), [vx 0 1 0]);
+
+% L = zeros(dm);
+% if dm(1)>=2
+%     tmp        = 1/(vx(1)^2);
+%     L(  1,1,1) = L(  1,1,1) + tmp*2;
+%     L(  2,1,1) = L(  2,1,1) - tmp;
+%     L(end,1,1) = L(end,1,1) - tmp;
+% end
+% if dm(2)>=2
+%     tmp        = 1/(vx(2)^2);
+%     L(1,  1,1) = L(1,  1,1) + tmp*2;
+%     L(1,  2,1) = L(1,  2,1) - tmp;
+%     L(1,end,1) = L(1,end,1) - tmp;
+% end
+% if dm(3)>=2
+%     tmp        = 1/(vx(3)^2);
+%     L(1,1,  1) = L(1,1,  1) + tmp*2;
+%     L(1,1,  2) = L(1,1,  2) - tmp;
+%     L(1,1,end) = L(1,1,end) - tmp;
+% end
+% L = single(fftn(L));
+% L = @(Y) real(ifftn(fftn(Y).*L));
 %==========================================================================
