a23a23b332b5c4b6574966927a361e0368e3326c
Mikael Brudfors <brudfors@gmail.com>
2019-02-04 15:11:56 +0000

    Enhancement(coreg_ims): Implemented alternating co-registration

--- private/coreg_ims.m
+++ private/coreg_ims.m
@@ -1,13 +1,17 @@
 function Nii = coreg_ims(Nii,dir_write)
-% Co-register images
+% Alternately co-register images so that each channel acts as the reference
+% image
 %__________________________________________________________________________
 % Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
 
-C = numel(Nii);
+C = numel(Nii); % Number of image channels
+
 if C == 1 && numel(Nii{1}) == 1
+    % Only one image, no need to co-register
     return;
 end
 
+% Make copies then collect all image in spm_vol object (V)
 V   = spm_vol;
 cnt = 1;
 for c=1:C
@@ -25,31 +29,33 @@ for c=1:C
     end
 end
 
-% Get image with smallest voxel size and pick this image as reference
-prod_vx = zeros(1,C);
-for i=1:numel(V)
-    vx         = sqrt(sum(V(i).mat(1:3,1:3).^2));    
-    prod_vx(i) = prod(vx);
-end
-[~,ref] = min(prod_vx);
-
-% Set options
-matlabbatch{1}.spm.spatial.coreg.estimate.ref               = {V(ref).fname};
+% Set registration options
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep      = [4 2];
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol      = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
 matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm     = [7 7];
 
-% Co-register
-source = 1:numel(V);
-source = source(source ~= ref);
-for i=source
-    matlabbatch{1}.spm.spatial.coreg.estimate.source = {V(i).fname}; 
-
-    spm_jobman('run',matlabbatch);
+% Start co-registration
+for ref=1:numel(V) % Iterate over all images
+    matlabbatch{1}.spm.spatial.coreg.estimate.ref = {V(ref).fname}; % Reference image to register to
     
-    [c,n]     = get_ix(Nii,i);
-    Nii{c}(n) = nifti(V(i).fname);
+    source = 1:numel(V);
+    source = source(source ~= ref);
+    for i=source % Iterate over all images, except reference
+        matlabbatch{1}.spm.spatial.coreg.estimate.source = {V(i).fname}; 
+
+        spm_jobman('run',matlabbatch);
+
+        V(i) = spm_vol(V(i).fname);
+    end
+end
+
+% Update NIfTIs, to make sure they have up-to-date orientation matrices
+for c=1:C
+    N = numel(Nii{c});
+    for n=1:N        
+        Nii{c}(n) = nifti(Nii{c}(n).dat.fname);
+    end
 end
 %==========================================================================
 
