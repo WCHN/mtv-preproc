6e391b2781ca7e2e1f6342bd2c61adf18d0e5eb1
Mikael Brudfors <brudfors@gmail.com>
2019-02-07 15:32:19 +0000

    Test: Added script to test estimation of rigid parameters

---
+++ TestEstimateRigid.m
@@ -0,0 +1,25 @@
+clear;
+
+dir_data = './SampleData';
+Nii      = nifti(spm_select('FPList',dir_data,'^.*\.nii$'));
+
+VoxelSize           = 1;
+Verbose             = 3;
+WorkersParfor       = Inf;
+
+IterMax             = 30;
+Tolerance           = 0;
+
+ADMMStepSize        = 0;
+RegScaleSuperResMRI = 20;
+DecreasingReg       = true;
+CoRegister          = false;
+EstimateRigid       = true;
+
+spm_mtv_preproc('InputImages',Nii,'Method','superres', ...
+                'VoxelSize',VoxelSize,'Verbose',Verbose, ...
+                'RegScaleSuperResMRI',RegScaleSuperResMRI, ...                
+                'ADMMStepSize',ADMMStepSize,'WorkersParfor',WorkersParfor, ...
+                'Tolerance',Tolerance,'IterMax',IterMax, ...
+                'DecreasingReg',DecreasingReg,'CoRegister',CoRegister, ...
+                'EstimateRigid',EstimateRigid);
