5825ef23e0cc46e2d41834efc755d0c53fd83499
Mikael Brudfors <brudfors@gmail.com>
2019-02-10 13:51:51 +0000

    Enhancement(compute_image_metrics): Use average

--- private/compute_image_metrics.m
+++ private/compute_image_metrics.m
@@ -1,29 +1,44 @@
-function [psnr1,ssim1] = compute_image_metrics(Nii,Nii_ref)
+function [avg_psnr,avg_ssim] = compute_image_metrics(Nii,Nii_ref)
 % Compute image quality metrics
 %
 %_______________________________________________________________________
 %  Copyright (C) 2018 Wellcome Trust Centre for Neuroimagin
 
-C = numel(Nii_ref);
+C     = numel(Nii_ref);
+psnrs = zeros(1,C);
+ssims = zeros(1,C);
 
-if iscell(Nii(1))
-    img = get_nii(Nii{1});
-else
-    img = get_nii(Nii(1));
-end
-ref = get_nii(Nii_ref(1));      
-
-for c=2:C
+for c=1:C % Loop over channels
     if iscell(Nii(c))
-        img1 = get_nii(Nii{c});
+        img = get_nii(Nii{c});
     else
-        img1 = get_nii(Nii(c));
+        img = get_nii(Nii(c));
     end
-
-    img = cat(3,img,img1);
-    ref = cat(3,ref,get_nii(Nii_ref(c)));                    
+    
+    ref = get_nii(Nii_ref(c));   
+    
+    psnrs(c) = get_psnr(img(:),ref(:));
+    ssims(c) = ssim(img,ref);    
 end
 
-psnr1 = get_psnr(img(:),ref(:));
-ssim1 = ssim(img,ref);
+avg_psnr = mean(psnrs);
+avg_ssim = mean(ssims);
+%==========================================================================
+
+%==========================================================================
+function val = get_psnr(hat,ref)
+% Calculates the peak signal-to-noise ratio (PSNR) for the image in array 
+% hat, with the image in array ref as the reference.
+% _______________________________________________________________________
+%  Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
+
+hat = double(hat(:)); 
+ref = double(ref(:));
+
+peak = max(hat);
+peak = max(peak,max(ref));
+
+RMSE = sqrt(mean((ref - hat).^2)); 
+
+val = 20*log10(peak/RMSE);
 %==========================================================================
