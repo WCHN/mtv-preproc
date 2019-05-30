d5ac42efc241f660c7cdd7d8bbea1dd00cdfcdb3
Mikael Brudfors <brudfors@gmail.com>
2019-05-30 16:54:05 +0100

    Enhancement(noise_estimate_ct)

--- private/noise_estimate_ct.m
+++ private/noise_estimate_ct.m
@@ -1,12 +1,11 @@
-function sd = noise_estimate_ct(Nii,speak,K)
+function [sd,info] = noise_estimate_ct(Nii,K)
 % Estimate noise in a CT image by fitting a variational Gaussian mixture
 % model (GMM) to a range of its intensity histogram, and taking the
 % component with a mean estimate closest to the value of air (HU = -1000)
 % _______________________________________________________________________
 %  Copyright (C) 2018 Wellcome Trust Centre for Neuroimaging
 
-if nargin < 2, speak = false; end
-if nargin < 3, K     = 3; end
+if nargin < 2, K = 2; end
    
 % Get image date (vectorised)
 f = Nii.dat(:);
@@ -15,28 +14,30 @@ f(f == min(f))  = [];
 f(f == max(f))  = [];
 
 % Remove some voxels
-mx = -800;
-mn = -1015;
+mx = -990;
+mn = -1023;
 f(f >= mx)    = [];
 f(f <  mn)    = [];
 
 % Histogram bin voxels
 x = min(f):max(f);
-c = hist(f(:),x);
+h = hist(f(:),x);
 
 % Transpose
 x = x';
-c = c';
+h = h';
 
 % Intensity distribution hyper-parameters
-MU0 = mean(f)*ones([1 K]) + K*randn([1 K]);
+% MU0 = mean(f)*ones([1 K]) + K*randn([1 K]);
+MU0 = [-1030 -1000];
 b0  = ones([1 K]);
 n0  = ones([1 K]);
 V0  = ones([1 1 K]);
 
 % Fit GMM
-Start       = -1000*ones([1 K]) + K*randn([1 K]);
-[~,MU,A,PI] = spm_gmm(x,K,c,'BinWidth',1,'GaussPrior',{MU0,b0,V0,n0}, ...
+% ['kmeans'],'linspace','prior','sample','uniform',
+% Start       = -1000*ones([1 K]) + K*randn([1 K]);
+[~,MU,A,PI] = spm_gmm(x,K,h,'BinWidth',1,'GaussPrior',{MU0,b0,V0,n0}, ...
                       'Tolerance',1e-6,'Start','prior','Verbose',0,'Prune',true);
 
 % Compute variance for each class
@@ -49,7 +50,7 @@ end
 [~,ix] = min(abs(abs(squeeze(MU)) - 1000));
 sd     = sqrt(squeeze(V(:,:,ix)));
 
-if speak
+if nargout >= 2
     % Plot histogram + GMM fit
     p = zeros([numel(x) K],'single');
     for k=1:K
@@ -58,10 +59,12 @@ if speak
     sp = sum(p,2) + eps;    
     md = mean(diff(x));
     
-    subplot(121)
-    plot(x(:),p,'--',x(:),c/sum(c)/md,'b.',x(:),sp,'r'); 
-    subplot(122)
-    hist(f(:),x')
-    drawnow
+    info    = struct;
+    info.x  = x;
+    info.f  = f;
+    info.h  = h;
+    info.p  = p;
+    info.sp = sp;
+    info.md = md;   
 end
 %==========================================================================
