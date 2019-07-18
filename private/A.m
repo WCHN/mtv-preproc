3fa68271debe423eb81bb5aa1263e503fb985adc
Mikael Brudfors <brudfors@gmail.com>
2019-07-18 09:35:24 +0100

    Enancement: New projection matrix

--- private/A.m
+++ private/A.m
@@ -13,46 +13,52 @@ end
 if nargin < 3
     X = cell(1,dat.N);
     for n=1:dat.N
-        R = dat.A(n).R;
-        T = dat.mat\R*dat.A(n).mat;
-        y = apply_affine(T,dat.A(n).dm);
+%         X{n} = apply_proj(Y,dat,n,'A');
+%         R = dat.A(n).R;
+%         T = dat.mat\R*dat.A(n).mat;
+%         y = apply_affine(T,dat.A(n).dm);
+% 
+%         if USE_PUSHPULL
+%             % Using pushpull.c
+%             if strcmp(dat.method,'superres')
+%                 X{n} = pushpull('pullc',Y,y,single(dat.A(n).J),double(dat.A(n).win));    
+%             elseif strcmp(dat.method,'denoise')
+%                 X{n} = spm_diffeo('pullc',Y,y);
+%             end
+%             clear y    
+%             X{n}(~isfinite(X{n})) = 0; 
+%         else
+%             % Using spm_diffeo
+%             tmp                   = real(ifftn(Y.*dat.A(n).S));    
+%             X{n}                  = spm_diffeo('pull',tmp,y);    
+%             X{n}(~isfinite(X{n})) = 0; 
+%             clear y tmp
+%         end
 
-        if USE_PUSHPULL
-            % Using pushpull.c
-            if strcmp(dat.method,'superres')
-                X{n} = pushpull('pullc',Y,y,single(dat.A(n).J),double(dat.A(n).win));    
-            elseif strcmp(dat.method,'denoise')
-                X{n} = spm_diffeo('pullc',Y,y);
-            end
-            clear y    
-            X{n}(~isfinite(X{n})) = 0; 
-        else
-            % Using spm_diffeo
-            tmp                   = real(ifftn(Y.*dat.A(n).S));    
-            X{n}                  = spm_diffeo('pull',tmp,y);    
-            X{n}(~isfinite(X{n})) = 0; 
-            clear y tmp
-        end
+        X{n} = apply_proj(Y,dat,n,'A');
     end
 else
-    R = dat.A(n).R;
-    T = dat.mat\R*dat.A(n).mat;
-    y = apply_affine(T,dat.A(n).dm);
+%     R = dat.A(n).R;
+%     T = dat.mat\R*dat.A(n).mat;
+%     y = apply_affine(T,dat.A(n).dm);
+% 
+%     if USE_PUSHPULL
+%         % Using pushpull.c
+%         if strcmp(dat.method,'superres')
+%             X = pushpull('pullc',Y,y,single(dat.A(n).J),double(dat.A(n).win));    
+%         elseif strcmp(dat.method,'denoise')
+%             X = spm_diffeo('pullc',Y,y);
+%         end
+%         clear y    
+%     else
+%         % Using spm_diffeo
+%         tmp             = real(ifftn(Y.*dat.A(n).S));    
+%         X               = spm_diffeo('pull',tmp,y);    
+%         X(~isfinite(X)) = 0; 
+%         clear y    
+%     end
 
-    if USE_PUSHPULL
-        % Using pushpull.c
-        if strcmp(dat.method,'superres')
-            X = pushpull('pullc',Y,y,single(dat.A(n).J),double(dat.A(n).win));    
-        elseif strcmp(dat.method,'denoise')
-            X = spm_diffeo('pullc',Y,y);
-        end
-        clear y    
-    else
-        % Using spm_diffeo
-        tmp             = real(ifftn(Y.*dat.A(n).S));    
-        X               = spm_diffeo('pull',tmp,y);    
-        X(~isfinite(X)) = 0; 
-        clear y    
-    end
+    X  = apply_proj(Y,dat,n,'A');
+%         Y1 = apply_proj(X,dat,n,'At');
 end
 %==========================================================================  
--- private/At.m
+++ private/At.m
@@ -10,53 +10,54 @@ if nargin < 3, tau = ones(1,dat.N); end
 if nargin < 4
     Y = single(0);   
     for n=1:dat.N      
-        R = dat.A(n).R;
-        T = dat.mat\R*dat.A(n).mat;
-        y = apply_affine(T,dat.A(n).dm);
-
-        if USE_PUSHPULL
-            % Using pushpull.c
-            if strcmp(dat.method,'superres')
-                tmp = pushpull('pushc',X{n},y,single(dat.A(n).J),double(dat.A(n).win),double(dat.dm));     
-            elseif strcmp(dat.method,'denoise')
-                tmp = spm_diffeo('pushc',X{n},y,dat.dm);
-            end
-            clear y
-            tmp(~isfinite(tmp)) = 0;
-        else
-            % Using spm_diffeo
-            tmp                 = spm_diffeo('push',X{n},y,dat.dm);     
-            clear y
-            tmp(~isfinite(tmp)) = 0;    
-            tmp                 = real(ifftn(fftn(tmp).*dat.A(n).S)); 
-            tmp(~isfinite(tmp)) = 0;
-        end
-        
-        Y = Y + tau(n).*tmp;           
+%         R = dat.A(n).R;
+%         T = dat.mat\R*dat.A(n).mat;
+%         y = apply_affine(T,dat.A(n).dm);
+% 
+%         if USE_PUSHPULL
+%             % Using pushpull.c
+%             if strcmp(dat.method,'superres')
+%                 tmp = pushpull('pushc',X{n},y,single(dat.A(n).J),double(dat.A(n).win),double(dat.dm));     
+%             elseif strcmp(dat.method,'denoise')
+%                 tmp = spm_diffeo('pushc',X{n},y,dat.dm);
+%             end
+%             clear y
+%             tmp(~isfinite(tmp)) = 0;
+%         else
+%             % Using spm_diffeo
+%             tmp                 = spm_diffeo('push',X{n},y,dat.dm);     
+%             clear y
+%             tmp(~isfinite(tmp)) = 0;    
+%             tmp                 = real(ifftn(fftn(tmp).*dat.A(n).S)); 
+%             tmp(~isfinite(tmp)) = 0;
+%         end
+        tmp = apply_proj(X{n},dat,n,'At');
+        Y   = Y + tau(n).*tmp;           
     end
 else
-    R = dat.A(n).R;
-    T = dat.mat\R*dat.A(n).mat;
-    y = apply_affine(T,dat.A(n).dm);
-
-    if USE_PUSHPULL
-        % Using pushpull.c
-        if strcmp(dat.method,'superres')
-            tmp = pushpull('pushc',X,y,single(dat.A(n).J),double(dat.A(n).win),double(dat.dm));     
-        elseif strcmp(dat.method,'denoise')
-            tmp = spm_diffeo('pushc',X,y,dat.dm);
-        end
-        clear y
-        tmp(~isfinite(tmp)) = 0;
-    else
-        % Using spm_diffeo
-        tmp                 = spm_diffeo('push',X,y,dat.dm);     
-        clear y
-        tmp(~isfinite(tmp)) = 0;    
-        tmp                 = real(ifftn(fftn(tmp).*dat.A(n).S)); 
-        tmp(~isfinite(tmp)) = 0;            
-    end
+%     R = dat.A(n).R;
+%     T = dat.mat\R*dat.A(n).mat;
+%     y = apply_affine(T,dat.A(n).dm);
+% 
+%     if USE_PUSHPULL
+%         % Using pushpull.c
+%         if strcmp(dat.method,'superres')
+%             tmp = pushpull('pushc',X,y,single(dat.A(n).J),double(dat.A(n).win),double(dat.dm));     
+%         elseif strcmp(dat.method,'denoise')
+%             tmp = spm_diffeo('pushc',X,y,dat.dm);
+%         end
+%         clear y
+%         tmp(~isfinite(tmp)) = 0;
+%     else
+%         % Using spm_diffeo
+%         tmp                 = spm_diffeo('push',X,y,dat.dm);     
+%         clear y
+%         tmp(~isfinite(tmp)) = 0;    
+%         tmp                 = real(ifftn(fftn(tmp).*dat.A(n).S)); 
+%         tmp(~isfinite(tmp)) = 0;            
+%     end
     
-    Y = tau(n).*tmp;      
+    tmp = apply_proj(X,dat,n,'At');
+    Y   = tau(n).*tmp;      
 end
 %==========================================================================
