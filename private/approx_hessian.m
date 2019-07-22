f281cc226b60171ccb7064016c74ee400691fcda
Mikael Brudfors <brudfors@gmail.com>
2019-07-22 10:27:49 +0100

    Mask Hessian

--- private/approx_hessian.m
+++ private/approx_hessian.m
@@ -1,4 +1,4 @@
-function Nii_H = approx_hessian(Nii_H,dat)
+function Nii_H = approx_hessian(Nii_H,Nii_x,dat)
 % Compute approximation to the diagonal of the Hessian 
 %
 %_______________________________________________________________________
@@ -7,8 +7,20 @@ function Nii_H = approx_hessian(Nii_H,dat)
 C = numel(dat);
 
 for c=1:C        
-    H        = At(A(ones(dat(c).dm,'single'),dat(c)),dat(c));   
+    A1 = A(ones(dat(c).dm,'single'),dat(c));
+    N  = numel(A1);
+    for n=1:N
+        X           = get_nii(Nii_x{c}(n));
+        msk         = get_msk(X);
+        clear X
+        A1{n}(~msk) = NaN;
+%         figure(111);imagesc3d(reshape(msk,dat(c).A.dm))
+    end
+    H        = At(A1,dat(c));   
+%     msk      = get_msk(H);
+%     figure(111);imagesc3d(H)
 %     H        = max(H(:))*ones(size(H),'single'); % Infinity norm
+%     H(~msk)  = 0;
     Nii_H(c) = put_nii(Nii_H(c),H);
 end    
 %==========================================================================
